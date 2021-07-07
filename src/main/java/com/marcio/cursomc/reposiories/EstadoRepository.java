b0f6c77bc2c2d159c6b2de83593bcbe6f32aec24
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-07 13:49:41 -0300

    cria entidade,relacionamento e repositorio de Estado e Cidade

---
+++ src/main/java/com/marcio/cursomc/reposiories/EstadoRepository.java
@@ -0,0 +1,15 @@
+package com.marcio.cursomc.reposiories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcio.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	//arquivp interface do JpaRepository que acessa o db
+	//recebe como parametro o nome da classe da tabela e o tipo do id da tabela
+	
+	
+
+}
